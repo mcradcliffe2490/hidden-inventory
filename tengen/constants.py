cc8dac7b298da0d18a0c55cd27b61266f547b765
Mycale C Radcliffe <40379734+mcradcliffe2490@users.noreply.github.com>
2024-08-17 14:23:09 -0400

    Update tcb site link

--- tengen/constants.py
+++ tengen/constants.py
@@ -6,6 +6,6 @@ from yarl import URL
 DEV_MODE = os.getenv("DEV_MODE")
 TENGEN_DIR = Path(__file__).parent
 ROOT = TENGEN_DIR.parent
-BASE_URL = URL("https://tcb-backup.bihar-mirchi.com")
+BASE_URL = URL("https://tcbscans.me")
 GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 DOCKER = os.getenv("DOCKER")
